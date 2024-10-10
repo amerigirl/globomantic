6e1d89987ed5461712453b8e6fda100cb3022d3a
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-10-10 16:24:39 -0700

    adjusting folder structure; working with styles

--- components/banner.js
+++ components/banner.js
@@ -1,11 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import {logo} from "./banner.module.css";
+
+const subtitleStyle= {
+fontStyle: "italic",
+fontSize: "x-large",
+color: "coral"
+}
+
 const Banner = () => {
     return (
-       <header>
-        <div>
-            <img src="./globomantics.png" alt="logo" />
+       <header className="row mb-4">
+        <div className="col-5">
+            <img src="./globomantics.png" alt="logo" className={logo} />
             
             </div>        
-        <div>
+        <div className="col-7 mt-5" style={subtitleStyle}>
             Providing houses all over the world
         </div>
 
